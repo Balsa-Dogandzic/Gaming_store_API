f98d1f58b0a6e22e5c833ed8cd49c388fdbd87d7
Your Name <you@example.com>
2022-08-11 14:28:16 +0200

    DEBUG on, and static url

--- game_store/game_store/settings.py
+++ game_store/game_store/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&d5wzoxfu@0h*duifc*&iq_y#toe62_2u#+-(bwoepdg@-kv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
